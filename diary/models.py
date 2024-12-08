731001e05c6192ec0f6119377a3d778876ea83d7
Zainab Alugo <zainabalugo@gmail.com>
2024-12-08 22:39:16 +0100

    Models

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Diary(models.Model):
+    date= models.DateTimeField (auto_now_add= True)
+    Title= models.CharField(max_length=50)
+    description= models.TextField()
+    
+    def __str__(self):
+        return self.date
